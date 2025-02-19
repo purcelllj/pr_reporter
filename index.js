87c77241a6a6fed31819d0fadb1159c584154f58
Liam Purcell <purcelllj@gmail.com>
2025-02-19 09:03:35 -0800

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+import { fetch } from 'undici';
+import fs from 'fs';
+import { config } from './config.js';
+
+const { author, token, org } = config;
+const uri =
+  `https://api.github.com/search/issues?q=org:${org}+is:pr+author:${author}+is:closed+closed:>${dateArg}`;
+const dateArg = process.argv[2];
+const headers = {
+  Authorization: token,
+  'User-Agent': author,
+  Accept: 'application/vnd.github+json',
+};
+
+const pullRequestResult = await fetch(uri, {
+  headers,
+});
+
+const json = await pullRequestResult.json();
+const mapped = json.items.map((i) => {
+  return {
+    title: i.title,
+    repository: i.repository_url.split('/').pop(),
+    pull_request_link: i.pull_request.url,
+    merged_at: i.pull_request.merged_at,
+  };
+});
+
+fs.writeFileSync(
+  'pullRequestReport.json',
+  JSON.stringify(mapped, undefined, 2),
+  { encoding: 'utf8' }
+);
